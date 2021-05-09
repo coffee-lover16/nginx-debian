532916d7fecd6af9de1071d06a5860182a6e73ae
Evgeniy Rotar <eugenio_161288@mail.ru>
2021-05-10 02:46:28 +0300

    update commands with mount cnfiguration

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 # build container
-docker build -t nginx1
+docker build -t nginx1 .
 # run container
 docker run --name nginx1 -d -p 8081:80/tcp --rm nginx1
+
+# run container with configuration file in /conf/nginx.conf -> /usr/local/nginx/conf/nginx.conf
+docker run --name nginx1 -d -p 8081:80/tcp --rm \
+ --mount type=bind,source="$(pwd)"/conf/nginx.conf,target=/usr/local/nginx/conf/nginx.conf \
+ nginx1
+
+# nginx releases:
+#
+# http://nginx.org/download/
